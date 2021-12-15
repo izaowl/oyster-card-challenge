cbff6385fd502e3f06a861c89d17d59311e4054d
izaowl <izasow@yahoo.com>
2021-12-15 01:20:10 +0000

    Done step 8, tests passing

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,8 +1,32 @@
 class Oystercard 
-attr_reader :balance
+attr_reader :balance, :in_journey
+LIMIT = 90
+FARE = 6
 
   def initialize
-    @balance
+    @balance = 0
+    @in_journey = false
+  end
+
+  def top_up(value)
+    raise "Error: max balance hit #{LIMIT}" if  (@balance+value>90) || @balance > LIMIT || value > LIMIT
+    @balance += value
+  end
+
+  def deduct
+    @balance = @balance - FARE
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+  
+  def touch_out
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
   end
 
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,10 +1,58 @@
 require 'oystercard'
 
 describe Oystercard do 
-  # let(:card) { Oystercard.new }
   
-  it 'has balance' do
-    expect(subject).to respond_to(:balance)
+  describe "#testing balance" do
+    it 'has balance' do
+      expect(subject).to respond_to(:balance)
+    end
+
+    it 'has balance = 0' do
+      expect(subject.balance).to eq (0)
+    end
+  end
+  describe "#testing top up" do
+
+    it 'can be topped up' do
+      expect { subject.top_up(5) }.to change {subject.balance}.from(0).to(5)
+    end
+
+    it 'checks top-up limit' do
+      expect {subject.top_up(91)}.to raise_error "Error: max balance hit #{Oystercard::LIMIT}"
+    end
+
+  end
+
+  describe "#testing deduct" do
+    it 'tests that fare is deducted from the card' do
+      card = Oystercard.new
+      card.top_up(10)
+      expect {card.deduct}.to change {card.balance}.from(10).to(4)
+    end
+  end
+
+  describe '#card in journey at the beginning' do
+    context 'in journey' do
+      it 'is true' do
+        expect(subject.in_journey?).to be false
+      end
+    end
+
+    context '#touching in' do
+      it 'checks if card "in journey" is true' do
+        card = Oystercard.new
+        card.touch_in
+        expect(card.in_journey?).to eq true
+      end
+    end
+
+    context '#touching out' do
+      it 'checks if card "in journey" is false' do
+        card = Oystercard.new
+        card.touch_out
+        expect(card.in_journey?).to eq false
+      end
+    end
   end
 
 
