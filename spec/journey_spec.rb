4621a072e16a3d1b909ee3d8b60a1f1ba46e5236
izaowl <izasow@yahoo.com>
2021-12-16 23:10:34 +0000

    Partially refactored code for step 14

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,5 +1,39 @@
 require 'journey'
-
 describe Journey do
+  subject(:journey) { described_class.new }
+  let(:station) { double :station }
 
+    describe "# - testing 3 stages" do
+    
+      it 'is initially not a journey' do
+        expect(subject).not_to be_in_journey
+      end 
+      it 'starting a journey' do
+        j = Journey.new
+        j.starting_journey(station)
+        expect(j).to be_in_journey 
+      end
+      
+      it 'starting a journey' do
+        j = Journey.new
+        j.ending_journey(station)
+        expect(j).to_not be_in_journey 
+      end
+    end
+
+    describe "#Calculate fare" do
+      it 'has a fare set by default' do
+        expect(subject.fare).to eq Journey::FARE
+      end
+    
+      it "confirms journey is complete" do
+        expect(subject.ending_journey(station)).to eq(station)
+      end
+
+      it "fare calculated based on the card being touched at the exit station" do
+        
+        expect(subject.calculate_penalty(:station)).to eq Journey::FARE
+      end  
+     
+    end
 end
